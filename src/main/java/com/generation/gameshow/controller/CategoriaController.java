4be2770085bcd7fbf05854d448490a50a18d0633
Edson Silva <dinhovdp@gmail.com>
2026-07-22 19:36:57 -0300

    :sparkles: categoria, controller e repository adicionados

--- src/main/java/com/generation/gameshow/controller/CategoriaController.java
+++ src/main/java/com/generation/gameshow/controller/CategoriaController.java
@@ -1,5 +1,66 @@
 package com.generation.gameshow.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.generation.gameshow.model.Categoria;
+import com.generation.gameshow.repository.CategoriaRepository;
+
+
+
+@RestController
+@RequestMapping("/categorias")
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Categoria>> getAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> getById(@PathVariable Long id) {
+
+        return repository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @GetMapping("/tipo/{tipo}")
+    public ResponseEntity<List<Categoria>> getByTipo(@PathVariable String tipo) {
+
+        return ResponseEntity.ok(repository.findAllByTipoContainingIgnoreCase(tipo));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> post(@RequestBody Categoria categoria) {
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(repository.save(categoria));
+
+    }
+
+    @PutMapping
+    public ResponseEntity<Categoria> put(@RequestBody Categoria categoria) {
+
+        return ResponseEntity.ok(repository.save(categoria));
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+
+        repository.deleteById(id);
+
+    }
 
 }
--- src/main/java/com/generation/gameshow/repository/CategoriaRepository.java
+++ src/main/java/com/generation/gameshow/repository/CategoriaRepository.java
@@ -1,5 +1,30 @@
 package com.generation.gameshow.repository;
 
-public class CategoriaRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.gameshow.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	
+	/* O JPA tem esses metodos integrados
+	 * save();
+		findAll();
+			findById();
+				deleteById();
+					existsById();
+	 */
+
+    
+    
+	
+    List<Categoria> findAllByTipoContainingIgnoreCase(String tipo);
+
+    /* A linha acima findbyConain..
+     * busca todas as categorias cujo campo tipo contenha esse texto, ignorando maiúsculas e minúsculas.
+     */
+
+    
+    
 }
